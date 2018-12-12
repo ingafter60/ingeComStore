75aba0afbfa142f21c35cb1b481df0e376521533
ing <inyoman_gurnitha@yahoo.com>
2018-12-13 05:16:16 +0700

    Modified index.php

--- index.php
+++ index.php
@@ -1,50 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>eCommerce Store</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" >
-  <link href="http://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100" rel="stylesheet" >
-	<link rel="stylesheet" href="styles/bootstrap.min.css">
-	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+   <meta charset="UTF-8">
+   <title>eCommerce Store</title>
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" >
+   <link href="http://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100" rel="stylesheet" >
+   <link rel="stylesheet" href="styles/bootstrap.min.css">
+   <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 </head>
 <body>
-	<!-- top Starts -->
-	<div id="top">
-		<!-- container Starts -->
-		<div class="container">
-			<!-- col-md-6 offer Starts -->
-		  <div class="col-md-6 offer">
-		     <a href="#" class="btn btn-success btn-sm">Welcome :Guest</a>
-		     <a href="#">Shopping Cart Total Price: $279, Total Items 3</a>
-		  </div>
-		  <!-- col-md-6 offer Ends -->
-		  <!-- col-md-6 Starts -->
-		  <div class="col-md-6">
-		    <!-- col-md-6 Starts -->
-		    <ul class="menu">
-					<!-- menu Starts -->
-					<li>
-					  <a href="customer_register.php">Register</a>
-					</li>
-					<li>
-					  <a href="checkout.php">My Account</a>                  
-						</li>
-					<li>
-					  <a href="cart.php">Go to Cart</a>
-					</li>
-					<li>
-					  <a href="checkout.php"> Login </a>                  
-						</li>
-				</ul>
-				<!-- menu Ends -->
-		  </div>
-		  <!-- col-md-6 Ends -->
-		</div>
-		<!-- container Ends -->
-  </div>
-	<!-- top Ends -->	
-	<script src="js/jquery.min.js"> </script>
-  <script src="js/bootstrap.min.js"></script>
+   <!-- top Starts -->
+   <div id="top">
+      <!-- container Starts -->
+      <div class="container">
+         <!-- col-md-6 offer Starts -->
+         <div class="col-md-6 offer">
+            <a href="#" class="btn btn-success btn-sm">Welcome :Guest</a>
+            <a href="#">Shopping Cart Total Price: $279, Total Items 3</a>
+         </div>
+         <!-- col-md-6 offer Ends -->
+         <!-- col-md-6 Starts -->
+         <div class="col-md-6">
+            <!-- col-md-6 Starts -->
+            <ul class="menu">
+               <!-- menu Starts -->
+               <li>
+                  <a href="customer_register.php">Register</a>
+               </li>
+               <li>
+                  <a href="checkout.php">My Account</a>                  
+               </li>
+               <li>
+                  <a href="cart.php">Go to Cart</a>
+               </li>
+               <li>
+                  <a href="checkout.php"> Login </a>                  
+               </li>
+            </ul>
+            <!-- menu Ends -->
+         </div>
+         <!-- col-md-6 Ends -->
+      </div>
+      <!-- container Ends -->
+   </div>
+   <!-- top Ends -->	
+   <script src="js/jquery.min.js"> </script>
+   <script src="js/bootstrap.min.js"></script>
 </body>
 </html>

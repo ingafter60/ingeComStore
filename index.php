8ae440a5cf076cbe924d46dbbaf57f7c6b90b617
ing <inyoman_gurnitha@yahoo.com>
2018-12-13 04:56:03 +0700

    Added Bootstrap, jQuery, and Font Awesome

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<title>eCommerce Store</title>
+	<link rel="stylesheet" href="styles/bootstrap.min.css">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 </head>
 <body>
 	<h1>Hello ...</h1>
+
+	<script src="js/jquery.min.js"> </script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
